aa0f9509a035082a01abb417544625cd6fa6a659
jubicudis <jubicudis@gmail.com>
2025-05-20 17:04:40 -0500

    refactor: Simplify user service methods and enhance context retrieval

--- pkg/github/users.go
+++ pkg/github/users.go
@@ -57,7 +57,6 @@ func (s *UserService) GetAuthenticated() (*User, error) {
 	err := s.client.Request(context.Background(), http.MethodGet, UserEndpoint, nil, user)
 	return user, err
 }
-}
 
 func (s *UserService) Get(username string) (*User, error) {
 	// WHO: UserGetter
@@ -73,8 +72,6 @@ func (s *UserService) Get(username string) (*User, error) {
 	err := s.client.Request(context.Background(), http.MethodGet, path, nil, user)
 	return user, err
 }
-}
-
 func (s *UserService) ListFollowers(username string) ([]User, error) {
 	// WHO: FollowerLister
 	// WHAT: List user followers
@@ -89,8 +86,8 @@ func (s *UserService) ListFollowers(username string) ([]User, error) {
 	err := s.client.Request(context.Background(), http.MethodGet, path, nil, &followers)
 	return followers, err
 }
-}
-
+	// WHO: FollowingLister
+	// WHAT: List users being followed
 func (s *UserService) ListFollowing(username string) ([]User, error) {
 	// WHO: FollowingLister
 	// WHAT: List users being followed
@@ -105,9 +102,7 @@ func (s *UserService) ListFollowing(username string) ([]User, error) {
 	err := s.client.Request(context.Background(), http.MethodGet, path, nil, &following)
 	return following, err
 }
-}
 
-// GetContext retrieves the 7D context for user operations
 func (s *UserService) GetContext() map[string]interface{} {
 	// WHO: ContextProvider
 	// WHAT: Get user service context
@@ -118,10 +113,25 @@ func (s *UserService) GetContext() map[string]interface{} {
 	// EXTENT: Service context
 
 	if s.client == nil {
-		return nil
+		// Create a 7D context map for user operations
+		return map[string]interface{}{
+			"who":    "UserService",
+			"what":   "UserOperation",
+			"when":   "CurrentTime",
+			"where":  "SystemLayer6",
+			"why":    "UserManagement",
+			"how":    "GitHubAPI",
+			"extent": "UserContext",
+		}
 	}
-
-	// Create a 7D context map for user operations
+	
+	// Return context with client information
+	userAgent := "unknown"
+	if s.client != nil {
+		if uaGetter, ok := interface{}(s.client).(interface{ UserAgent() string }); ok {
+			userAgent = uaGetter.UserAgent()
+		}
+	}
 	return map[string]interface{}{
 		"who":    "UserService",
 		"what":   "UserOperation",
@@ -130,5 +140,9 @@ func (s *UserService) GetContext() map[string]interface{} {
 		"why":    "UserManagement",
 		"how":    "GitHubAPI",
 		"extent": "UserContext",
+		"client": map[string]interface{}{
+			"baseURL":   s.client.baseURL.String(),
+			"userAgent": userAgent,
+		},
 	}
 }
