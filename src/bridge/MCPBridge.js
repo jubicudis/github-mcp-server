22f5076566c1fd40dfec27931acc5d77b051e31a
jubicudis <jubicudis@gmail.com>
2025-05-27 16:17:23 -0500

    updates

--- src/bridge/MCPBridge.js
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left as a reference. The actual MCP bridge implementation is in:
-// ../src/typescript/neuro/bridge/MCPBridge.js
-module.exports = require('../typescript/neuro/bridge/MCPBridge.js');
